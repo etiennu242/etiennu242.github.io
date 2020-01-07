3e384da19a164d33933e8b6860ff2e4ba18c6c22
etiennu242 <copperhusky242@gmail.com>
2020-01-07 20:32:15 +0300

    comic added

---
+++ comic.js
@@ -0,0 +1,24 @@
+var i = 1;
+var imgs = ['img/comic/1.jpg','img/comic/2.jpg', 'img/comic/3.jpg', 'img/comic/4.jpg', 'img/comic/5.jpg', 'img/comic/6.jpg', 'img/comic/7.jpg', 'img/comic/8.jpg', 'img/comic/9.jpg', 'img/comic/10.jpg', 'img/comic/11.jpg', 'img/comic/12.jpg', 'img/comic/13.jpg', 'img/comic/14.jpg', 'img/comic/15.jpg', 'img/comic/16.jpg', 'img/comic/17.jpg', 'img/comic/18.jpg', 'img/comic/19.jpg', 'img/comic/20.jpg', 'img/comic/21.jpg', 'img/comic/22.jpg', 'img/comic/23.jpg', 'img/comic/24.jpg', 'img/comic/25.jpg', 'img/comic/26.jpg'];
+
+
+function slider(act) {
+    if(act === '>'){
+        if(i !== imgs.length - 1){
+            i++;
+        }
+    } else if (i > 0){
+        i--;
+    }
+
+    document.getElementById('comic').src = imgs[i];
+}
+
+document.onkeydown = function(key) {
+    if (key.keyCode === 37) {
+        slider('<');
+    }
+    if (key.keyCode === 39) {
+        slider('>');
+    }
+}
